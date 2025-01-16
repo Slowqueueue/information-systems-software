acf906c7c8c3e1f50d107a1bee2c0706cb2b2317
Nikita Pertcevoy <nikita.nikita2002@bk.ru>
2025-01-16 12:28:19 +0700

    Add files via upload

---
+++ lab1/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                string expression = Request.Form["expr"];
+                try
+                {
+                    double result = Convert.ToDouble(new DataTable().Compute(expression, null));
+                    answer.Text = result.ToString();
+                }
+                catch (Exception err)
+                {
+                    answer.Text = "(Ошибка: " + err.Message + ')';
+                }
+            }
+        }
+    }
+}
